b801ee30435f90aaa60a3e5a049f67a1ac288d85
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-03-14 08:32:08 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+// Multiply
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+
+//Random
+let random = Math.floor(Math.random() * 10) + 1;
+
+//Mod
+let num3 = 22
+let num4 = 18
+let mod = (22 % 18)
+
+//Max
+let max = Math.max(16,17,18,19,20);
+
+
